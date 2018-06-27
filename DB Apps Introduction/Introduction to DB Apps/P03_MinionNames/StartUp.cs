1877f7e3e3e7c538a0e001cadcf51fd563e4461c
deyanslavov <pikacho2013@gmail.com>
2018-06-27 19:56:01 +0300

    refactored most of the code into methods

--- DB Apps Introduction/Introduction to DB Apps/P03_MinionNames/StartUp.cs
+++ DB Apps Introduction/Introduction to DB Apps/P03_MinionNames/StartUp.cs
@@ -38,30 +38,36 @@
                     Console.WriteLine($"Villain: {villainName}");
                 }
 
-                string getAllVillainMinionsQuery = "SELECT m.Name, m.Age FROM Minions AS m JOIN MinionsVillains AS mv ON mv.MinionId = m.Id JOIN Villains AS v ON v.Id = mv.VillainId WHERE v.Id = @villainId ORDER BY m.Name";
-                var getAllVillainMinionsQueryCmd = new SqlCommand(getAllVillainMinionsQuery, connection);
-                getAllVillainMinionsQueryCmd.Parameters.AddWithValue("@villainId", villainId);
-
-                reader.Close();
-                reader = getAllVillainMinionsQueryCmd.ExecuteReader();
-                int minionPosition = 1;
-
-                if (!reader.Read())
-                {
-                    Console.WriteLine("(no minions)");
-                }
-                else
-                {
-                    while (reader.Read())
-                    {
-                        string minionName = (string)reader[0];
-                        var minionAge = reader[1];
-
-                        Console.WriteLine($"{minionPosition++}. {minionName} {minionAge}");
-                    }
-                }
-                reader.Close();
+                reader = PrintMinions(villainId, connection, reader);
             }
+        }
+
+        private static SqlDataReader PrintMinions(int villainId, SqlConnection connection, SqlDataReader reader)
+        {
+            string getAllVillainMinionsQuery = "SELECT m.Name, m.Age FROM Minions AS m JOIN MinionsVillains AS mv ON mv.MinionId = m.Id JOIN Villains AS v ON v.Id = mv.VillainId WHERE v.Id = @villainId ORDER BY m.Name";
+            var getAllVillainMinionsQueryCmd = new SqlCommand(getAllVillainMinionsQuery, connection);
+            getAllVillainMinionsQueryCmd.Parameters.AddWithValue("@villainId", villainId);
+
+            reader.Close();
+            reader = getAllVillainMinionsQueryCmd.ExecuteReader();
+            int minionPosition = 1;
+
+            if (!reader.Read())
+            {
+                Console.WriteLine("(no minions)");
+            }
+            else
+            {
+                while (reader.Read())
+                {
+                    string minionName = (string)reader[0];
+                    var minionAge = reader[1];
+
+                    Console.WriteLine($"{minionPosition++}. {minionName} {minionAge}");
+                }
+            }
+            reader.Close();
+            return reader;
         }
     }
 }
