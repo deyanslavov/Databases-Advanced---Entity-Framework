02f362d633b20f778ae94dd5ac4204b4df5b42a1
deyanslavov <pikacho2013@gmail.com>
2018-06-27 19:56:32 +0300

    refactored most of the code into methods

--- DB Apps Introduction/Introduction to DB Apps/P05_ChangeTownNamesCasing/StartUp.cs
+++ DB Apps Introduction/Introduction to DB Apps/P05_ChangeTownNamesCasing/StartUp.cs
@@ -3,7 +3,6 @@
     using System;
     using System.Collections.Generic;
     using System.Data.SqlClient;
-    using System.Text;
 
     public class StartUp
     {
@@ -16,11 +15,10 @@
             connection.Open();
             using (connection)
             {
-                string getCountryIdQuery = "SELECT Id FROM Countries WHERE Name = @countryName";
-                var getCountryIdQueryCmd = new SqlCommand(getCountryIdQuery, connection);
-                getCountryIdQueryCmd.Parameters.AddWithValue("@countryName", countryName);
+                SqlCommand getCountryIdQueryCmd;
+                SqlDataReader reader;
 
-                var reader = getCountryIdQueryCmd.ExecuteReader();
+                GetCountryId(countryName, connection, out getCountryIdQueryCmd, out reader);
 
                 if (!reader.Read())
                 {
@@ -33,29 +31,55 @@
                     int countryId = (int)getCountryIdQueryCmd.ExecuteScalar();
                     reader.Close();
 
-                    string changeTownNamesCasingQuery = "UPDATE Towns SET Name = UPPER(Name) WHERE CountryId = @countryId";
-                    var changeTownNamesCasingQueryCmd = new SqlCommand(changeTownNamesCasingQuery, connection);
-                    changeTownNamesCasingQueryCmd.Parameters.AddWithValue("@countryId", countryId);
+                    int rowsAffected = ChangeTownNamesCasing(connection, countryId);
 
-                    int rowsAffected = changeTownNamesCasingQueryCmd.ExecuteNonQuery();
-
-                    string getTownNamesQuery = "SELECT Name FROM Towns WHERE CountryId = @countryId";
-                    var getTownNamesQueryCmd = new SqlCommand(getTownNamesQuery, connection);
-                    getTownNamesQueryCmd.Parameters.AddWithValue("@countryId", countryId);
+                    SqlCommand getTownNamesQueryCmd = CreateGetTownNamesCmd(connection, countryId);
 
                     reader = getTownNamesQueryCmd.ExecuteReader();
 
-                    var towns = new List<string>();
-                    while (reader.Read())
-                    {
-                        towns.Add((string)reader[0]);
-                    }
-                    reader.Close();
-
-                    Console.WriteLine($"{rowsAffected} town names were affected.");
-                    Console.WriteLine("[" + string.Join(", ", towns) + "]");
+                    PrintOutput(reader, rowsAffected);
                 }
             }
+        }
+
+        private static void PrintOutput(SqlDataReader reader, int rowsAffected)
+        {
+            var towns = new List<string>();
+            while (reader.Read())
+            {
+                towns.Add((string)reader[0]);
+            }
+            reader.Close();
+
+            Console.WriteLine($"{rowsAffected} town names were affected.");
+            Console.WriteLine("[" + string.Join(", ", towns) + "]");
+        }
+
+        private static SqlCommand CreateGetTownNamesCmd(SqlConnection connection, int countryId)
+        {
+            string getTownNamesQuery = "SELECT Name FROM Towns WHERE CountryId = @countryId";
+            var getTownNamesQueryCmd = new SqlCommand(getTownNamesQuery, connection);
+            getTownNamesQueryCmd.Parameters.AddWithValue("@countryId", countryId);
+            return getTownNamesQueryCmd;
+        }
+
+        private static int ChangeTownNamesCasing(SqlConnection connection, int countryId)
+        {
+            string changeTownNamesCasingQuery = "UPDATE Towns SET Name = UPPER(Name) WHERE CountryId = @countryId";
+            var changeTownNamesCasingQueryCmd = new SqlCommand(changeTownNamesCasingQuery, connection);
+            changeTownNamesCasingQueryCmd.Parameters.AddWithValue("@countryId", countryId);
+
+            int rowsAffected = changeTownNamesCasingQueryCmd.ExecuteNonQuery();
+            return rowsAffected;
+        }
+
+        private static void GetCountryId(string countryName, SqlConnection connection, out SqlCommand getCountryIdQueryCmd, out SqlDataReader reader)
+        {
+            string getCountryIdQuery = "SELECT Id FROM Countries WHERE Name = @countryName";
+            getCountryIdQueryCmd = new SqlCommand(getCountryIdQuery, connection);
+            getCountryIdQueryCmd.Parameters.AddWithValue("@countryName", countryName);
+
+            reader = getCountryIdQueryCmd.ExecuteReader();
         }
     }
 }
